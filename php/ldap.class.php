ba82b5daee62c3fd23028d3ee48700eaa22c554b
Andrew Wetmore <Alwaygetgo@gmail.com>
2016-03-16 19:05:39 -0400

    Uses only Evals Database to determine users on floor (Instead of LDAP).

--- php/ldap.class.php
+++ php/ldap.class.php
@@ -4,7 +4,7 @@
 	class LdapHelper {
 		private $ldap_conn = null;
 
-		private $FIND_ALL_MEMBERS_QUERY =  '(&(onfloor=1)(objectClass=houseMember))';
+		private $FIND_ALL_MEMBERS_QUERY =  '(&(objectClass=houseMember))';
 		private $FIND_ALL_MEMBERS_FIELDS = array('nickname',
 		                                         'roomNumber',
 		                                         'cn',
@@ -21,15 +21,16 @@
 		}
 
 		function connect($ldap_url) {
-			# Set some LDAP options.
-			ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 			# Connect to LDAP.
 			$this->ldap_conn = ldap_connect($ldap_url);
+			# Set some LDAP options.
+			ldap_set_option($this->ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 			# Bind to LDAP
 			ldap_bind($this->ldap_conn, 'uid='.LDAP_USER.',ou=Users,dc=csh,dc=rit,dc=edu', LDAP_PASS);
 			
+			/*ldap_bind($this->ldap_conn);
 			# Try to log in using the currently authenticated Webauth user.
-			/*if(isset($_ENV["KRB5CCNAME"]))
+			if(isset($_ENV["KRB5CCNAME"]))
 			{
 				putenv("KRB5CCNAME=" . getenv("KRB5CCNAME"));
 				ldap_sasl_bind($this->ldap_conn,"","","GSSAPI") or die("Could not bind to LDAP 2: ");
@@ -98,11 +99,6 @@
 			foreach ($onfloors as $person) {
 				if (!isset($person['dn']))
 					continue;
-					
-				# Get the year level from the home directory
-				$matches = null;
-				preg_match('/^(\d{4})/', $person['membersince'][0], $matches);
-				$yearLevel = $curyear - $matches[1] + 1;
 
 				# Get the user name from the DN
 				preg_match('/^uid=(.*),o.*\z/', $person['dn'], $matches);
@@ -110,6 +106,11 @@
 
 				if(isset($roster[$username]))
 				{
+					# Get the year level from the home directory
+					$matches = null;
+					preg_match('/^(\d{4})/', $person['membersince'][0], $matches);
+					$yearLevel = $curyear - $matches[1] + 1;
+					
 					# echo '"'.$person['dn'].'"<br>';
 					$members[$person['dn']] = array(
 						'name' => $person['cn'][0],
